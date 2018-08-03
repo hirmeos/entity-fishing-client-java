eb8b3ee6ed7258b13bc5ee2ddcf4ccd96a344ea4
tantikristanti <tantikristanti@gmail.com>
2018-08-03 16:25:36 +0200

    methods for text and term disambiguation

--- src/main/java/com/scienceminer/nerd/client/NerdClient.java
+++ src/main/java/com/scienceminer/nerd/client/NerdClient.java
@@ -2,24 +2,33 @@ package com.scienceminer.nerd.client;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 
@@ -49,8 +58,30 @@ public class NerdClient {
         this.port = port; 
     }
 
-    public String getConcept() {
-        throw new UnsupportedOperationException("Method not yet implemented.");
+    public String getConcept(String id) {
+
+        String response = null;
+        String urlNerd = "http://" + this.host + PATH_CONCEPT + "/" + id;
+        if ((id != null) || (id.startsWith("Q") || (id.startsWith("P")))) {
+            try {
+                HttpClient client = HttpClientBuilder.create().build();
+
+                HttpGet request = new HttpGet(urlNerd);
+                HttpResponse httpResponse = client.execute(request);
+                HttpEntity entity = httpResponse.getEntity();
+
+                int responseId = httpResponse.getStatusLine().getStatusCode();
+                if (responseId == HttpStatus.SC_OK) {
+                    response = IOUtils.toString(entity.getContent(), StandardCharsets.UTF_8);
+                    return response;
+                } else {
+                    return response;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return response;
     }
 
 
@@ -59,21 +90,19 @@ public class NerdClient {
     }
 
 
-
     public String segment() {
         throw new UnsupportedOperationException("Method not yet implemented.");
     }
 
 
-    public String disambiguateText(String text, String language) {
+    public String textDisambiguate(String text, String language) {
 
+        String result = null;
         try {
             final URI uri = new URIBuilder()
                     .setScheme("http")
-                    .setPort(port)
                     .setHost(this.host + PATH_DISAMBIGUATE)
                     .build();
-
 
             ObjectMapper mapper = new ObjectMapper();
             ObjectNode node = mapper.createObjectNode();
@@ -83,30 +112,87 @@ public class NerdClient {
                 dataNode.put("lang", language);
                 node.set("language", dataNode);
             }
+            HttpPost httpPost = new HttpPost(uri);
+            CloseableHttpClient httpResponse = HttpClients.createDefault();
 
+            httpPost.setHeader("Content-Type", APPLICATION_JSON.toString());
+            httpPost.setEntity(new StringEntity(node.toString()));
+            CloseableHttpResponse closeableHttpResponse = httpResponse.execute(httpPost);
 
-            HttpPost post = new HttpPost(uri);
-            CloseableHttpClient httpclient = HttpClients.createDefault();
-            post.setHeader("Content-Type", APPLICATION_JSON.toString());
-            post.setEntity(new StringEntity(node.toString()));
-            CloseableHttpResponse response = httpclient.execute(post);
-
-            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                return IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+            if (closeableHttpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                return IOUtils.toString(closeableHttpResponse.getEntity().getContent(), StandardCharsets.UTF_8);
             } else {
-                LOGGER.error("Error disambiguating text: " + response.getStatusLine().getStatusCode());
-                return null;
+                return result;
             }
-        } catch (MalformedURLException e) {
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    public String termDisambiguate(Map<String, Double> listOfTerm, String language) {
+
+        String result = null, term = null;
+        double score = 0.0;
+        try {
+            final URI uri = new URIBuilder()
+                    .setScheme("http")
+                    .setHost(this.host + PATH_DISAMBIGUATE)
+                    .build();
+
+
+            ObjectMapper mapper = new ObjectMapper();
+            ObjectNode node = mapper.createObjectNode();
+
+            ArrayNode termsNode = mapper.createArrayNode();
+
+            for (Map.Entry<String, Double> list : listOfTerm.entrySet()) {
+                term = list.getKey();
+                score = list.getValue();
+                ObjectNode termNode = mapper.createObjectNode();
+                termNode.put("term", term);
+                termNode.put("score", score);
+                termsNode.add(termNode);
+            }
+
+            node.set("termVector", termsNode);
+
+            if (language != null) {
+                ObjectNode dataNode = mapper.createObjectNode();
+                dataNode.put("lang", language);
+                node.set("language", dataNode);
+            }
+            HttpPost httpPost = new HttpPost(uri);
+            CloseableHttpClient httpResponse = HttpClients.createDefault();
+
+            httpPost.setHeader("Content-Type", APPLICATION_JSON.toString());
+            httpPost.setEntity(new StringEntity(node.toString()));
+            CloseableHttpResponse closeableHttpResponse = httpResponse.execute(httpPost);
+
+            if (closeableHttpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                return IOUtils.toString(closeableHttpResponse.getEntity().getContent(), StandardCharsets.UTF_8);
+            } else {
+                return result;
+            }
+
+        } catch (URISyntaxException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
         }
-        return null;
+        return result;
     }
+
+
 
 }
